b1aeff3dcb4c8366df6b627c20ce0cd1ee705f7f
Ron31 <ron3108@gmx.net>
2019-01-17 18:21:19 +0100

    Fixed a typo.

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-modules.export = async (client, message) => {
+module.export = async (client, message) => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") {
         return message.channel.send(`[INFO] DM Commands are still work in progress`);
