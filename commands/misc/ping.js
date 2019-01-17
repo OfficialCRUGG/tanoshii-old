4696fcce34f8ded3d6fd2971e4d811a7bccc7a45
OfficialCRUGG <mrcreeper2011@web.de>
2019-01-17 19:40:22 +0100

    Added ping command

---
+++ commands/misc/ping.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
+  const msg = await message.channel.send(`${client.guilds.get('522816995406643227').emojis.find("name", "loading")} Checking ping...`);
+  if(msg.createdTimestamp - message.createdTimestamp > 300) {
+      msg.edit(`${client.guilds.get('522816995406643227').emojis.find("name", "statusred")} The bot latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  } else if(msg.createdTimestamp - message.createdTimestamp > 200) {
+      msg.edit(`${client.guilds.get('522816995406643227').emojis.find("name", "statusyellow")} The bot latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  } else if(msg.createdTimestamp - message.createdTimestamp <= 100) {
+      msg.edit(`${client.guilds.get('522816995406643227').emojis.find("name", "statusgreen")} The bot latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+}
+
+module.exports.help = {
+  name: "ping"
+}
