83ef2917f109807a8dddeb7565294f680124b23e
OfficialCRUGG <mrcreeper2011@web.de>
2019-01-19 13:10:17 +0100

    Minor changes to index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-////////////////////////////////////
-//                                //
-//    To be developed by Ron31    //
-//                                //
-////////////////////////////////////
-
-// temporary index.js file
 const config = require("./config.json");
 const tokenFile = require("./token.json");
 const Discord = require("discord.js");
@@ -16,11 +9,11 @@ client.commands = new Discord.Collection();
 client.groups = [];
 
 for (let dir of cmdDir) {
-    console.log("- Loading group " + dir);
+    console.log("[INFO] Loading command category " + dir ".");
     client.groups.push(dir);
     let group = fs.readdirSync(`./commands/${dir}`);
     for (let commandFile of group) {
-        console.log("-- Loading command " + commandFile.split(".")[0] + " of " + dir);
+        console.log("[INFO] Loading command " + dir + "/" + commandFile.split(".")[0] + ".");
         if (!commandFile.endsWith('.js')) return;
         let command = require(`./commands/${dir}/${commandFile}`);
         client.commands.set(commandFile.split('.')[0], command);
@@ -30,7 +23,7 @@ fs.readdir("./events", (err, files) => {
     if (err) return console.error(err);
     files.forEach(file => {
         if (!file.endsWith(".js")) return;
-        console.log(`- Loading file ${file.split('.')[0]}`);
+        console.log(`[INFO] Loading event ${file.split('.')[0]}.`);
         const event = require(`./events/${file}`);
         let eventName = file.split(".")[0];
         client.on(eventName, event.bind(null, client));
