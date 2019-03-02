903255a8a84ae348b688804ec9b92b9ec339f6f4
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-02 22:10:16 +0100

    Added devmessage command

---
+++ commands/misc/devmessage.js
@@ -0,0 +1,42 @@
+// This command is based on the Rhodium Bot licensed under the Apache License 2.0 - https://github.com/RhodiumBot/Rhodium/blob/master/commands/core/devmsg.js
+
+const Discord = require("discord.js");
+
+function sendSuccess(config, client, cmd, author, message) {
+  let embed = new Discord.RichEmbed()
+  .setTitle("Sent.")
+  .setColor(config.mainColor)
+  .setDescription("Your message has been sent to the developers.")
+  .setFooter(`${client.user.username} ● ${cmd} ● Requested by ${author.tag}`)
+  return message.channel.send(embed);
+}
+
+function sendError(config, client, cmd, author, message) {
+  let embed = new Discord.RichEmbed()
+  .setTitle("An error occured.")
+  .setColor(config.mainColor)
+  .setDescription("It appears you haven't entered a message.")
+  .setFooter(`${client.user.username} ● ${cmd} ● Requested by ${author.tag}`)
+  return message.channel.send(embed);
+}
+
+module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
+  if(args.join(' ') !== ""){
+        client.config.developers.forEach(d => {
+          let embed = new Discord.RichEmbed()
+          .setTitle("New Developer Message")
+          .setColor(config.mainColor)
+          .setDescription("```" + args.join(' ') + "```")
+          .setFooter(`${client.user.username} ● Sent by ${author.tag} ● User ID: ${author.id}`)
+          return client.users.get(d).send(embed)
+        });
+        sendSuccess(config, client, cmd, author, message);
+        message.delete()
+    } else {
+      sendError(config, client, cmd, author, message);
+    }
+}
+
+module.exports.help = {
+  name: "help"
+}
