e2431195cde5073a251adf35d6bafa763c601974
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-03 15:10:00 +0100

    'lmgtfy' & 'gtfm' now use shortlinks

--- commands/misc/gtfm.js
+++ commands/misc/gtfm.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js");
 const encode = require("strict-uri-encode");
+const isgd = require("isgd");
 
 module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
   let question = encode(args.join(" "));
   let link = `https://www.google.com/search?q=${question}`;
-  message.channel.send(`Okay. Just googled that for you. There you are: **<${link}>**`);
+  isgd.shorten(link, function(res) {
+    message.channel.send(`Okay. Just googled that for you. There you are: **<${res}>**`);
+  });
 };
 
 module.exports.help = {
--- commands/misc/lmgtfy.js
+++ commands/misc/lmgtfy.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js");
 const encode = require("strict-uri-encode");
+const isgd = require("isgd");
 
 module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
   let question = encode(args.join(" "));
   let link = `http://www.lmgtfy.com/?q=${question}`;
-  message.channel.send(`**<${link}>**`);
+  isgd.shorten(link, function(res) {
+    message.channel.send(`**<${res}>**`);
+  });
 };
 
 module.exports.help = {
