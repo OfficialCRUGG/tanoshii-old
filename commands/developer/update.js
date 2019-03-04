486833043a2f35e32d2864ae855146f01c5169e4
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-04 23:03:09 +0100

    Hopefully final fix for update

--- commands/developer/update.js
+++ commands/developer/update.js
@@ -1,25 +1,5 @@
 // Source by Rhodium https://github.com/RhodiumBot/Rhodium/blob/master/commands/vserver/update.js
 
-const downloadUpdate = require("../../utils/downloadUpdate.js")
-const installDependencies = require('../../utils/installDependencies.js');
-
-const Discord = require("discord.js");
-const { exec } = require("child_process");
-
-module.exports.run = async () => {
-  if(author.id = "228965621478588416" || "224084384054116352") {
-    downloadUpdate()
-    .then(installDependencies())
-    .then(process.exit(0));
-  } else {
-    message.reply("No permissions")
-  }
-};
-module.exports.help = {
-  name: "update"
-}
-
-
 const { exec } = require("child_process");
 const { RichEmbed } = require("discord.js");
 const fs = require("fs");
@@ -30,20 +10,18 @@ module.exports.run = async (prefix, messageArray, cmd, client, msg, args, author
         .setTitle(`Update initiated by ${msg.author.tag}`)
         .setDescription("Updating...");
 
-    let embmsg = await msg.channel.send(emb);
+    msg.channel.send(emb);
     client.user.setPresence({status: "online", game: {name: "Applying an update."}});
 
     exec("git pull", (err, out, stderr) => {
         if(!err && stderr === ""){
             console.log(out);
-            emb.description = emb.description.replace(`${client.vars.emojiIcons.animated.loading} Pulling`, `${client.vars.emojiIcons.check} Pulling`)
-            embmsg.edit(emb);
             exec("pm2 restart all", (err, out, stderr) => {
                 if(err && stderr !== "") {
                   let emb2 = new RichEmbed()
                   .setTitle(`Update failed`)
                   .setDescription(`There was an error\n${out}\n${stderr}`);
-                  let embmsg2 = await msg.channel.send(emb2);
+                  msg.channel.send(emb2);
                 }
             })
         }
@@ -51,7 +29,7 @@ module.exports.run = async (prefix, messageArray, cmd, client, msg, args, author
           let emb3 = new RichEmbed()
           .setTitle(`Update failed`)
           .setDescription(`There was an error\n${out}\n${stderr}`);
-          let embmsg3 = await msg.channel.send(emb3);
+          msg.channel.send(emb3);
         }
     })
   }
