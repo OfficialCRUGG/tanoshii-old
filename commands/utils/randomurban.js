b0d3059c23586c302438f81cd5cdf88f9758b1a9
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-06 15:48:46 +0100

    Made urban dictionary commands nsfw.

--- commands/utils/randomurban.js
+++ commands/utils/randomurban.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const urban = require("urban")
 
 module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
+  if(message.channel.nsfw) {
+
   urban.random().first(json => {
     let embed = new Discord.RichEmbed()
     .setTitle(json.word)
@@ -14,6 +16,15 @@ module.exports.run = async (prefix, messageArray, cmd, client, message, args, au
     .setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
     return message.channel.send(embed);
   });
+
+} else {
+  let embed = new Discord.RichEmbed()
+  .setTitle(`${config.name} - Neko`)
+  .setColor(config.mainColor)
+  .setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
+  .setDescription("Please use this command in an nsfw channel, as some results may be 18+")
+  return message.channel.send(embed)
+}
 };
 module.exports.help = {
   name: "randomurban"
--- commands/utils/urban.js
+++ commands/utils/urban.js
@@ -2,27 +2,39 @@ const Discord = require("discord.js");
 const urban = require("urban")
 
 module.exports.run = async (prefix, messageArray, cmd, client, message, args, author, guild, config) => {
-  if(args.length < 1) {
-    let embed = new Discord.RichEmbed().setTitle("An error occured.").setColor(config.mainColor).setDescription("It appears you haven't entered a search phrase.").setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
-    return message.channel.send(embed);
-  }
-  let str = args.join(" ");
-  urban(str).first(json => {
-    if(!json) {
-      let embed = new Discord.RichEmbed().setTitle("No results.").setColor(config.mainColor).setDescription("No results have been found.").setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
+
+  if(message.channel.nsfw) {
+
+    if(args.length < 1) {
+      let embed = new Discord.RichEmbed().setTitle("An error occured.").setColor(config.mainColor).setDescription("It appears you haven't entered a search phrase.").setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
       return message.channel.send(embed);
-    };
+    }
+    let str = args.join(" ");
+    urban(str).first(json => {
+      if(!json) {
+        let embed = new Discord.RichEmbed().setTitle("No results.").setColor(config.mainColor).setDescription("No results have been found.").setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
+        return message.channel.send(embed);
+      };
+      let embed = new Discord.RichEmbed()
+      .setTitle(json.word)
+      .setURL(json.permalink)
+      .setColor(config.mainColor)
+      .setDescription("Definition by *" + json.author + "*:\n**" + json.definition + "**")
+      .addField("Example", json.example, false)
+      .addField("Upvotes", json.thumbs_up, true)
+      .addField("Downvotes", json.thumbs_down, true)
+      .setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
+      return message.channel.send(embed);
+    });
+
+  } else {
     let embed = new Discord.RichEmbed()
-    .setTitle(json.word)
-    .setURL(json.permalink)
+    .setTitle(`${config.name} - Neko`)
     .setColor(config.mainColor)
-    .setDescription("Definition by *" + json.author + "*:\n**" + json.definition + "**")
-    .addField("Example", json.example, false)
-    .addField("Upvotes", json.thumbs_up, true)
-    .addField("Downvotes", json.thumbs_down, true)
     .setFooter(`${config.name} ● ${cmd} ● Requested by ${author.tag}`);
-    return message.channel.send(embed);
-  });
+    .setDescription("Please use this command in an nsfw channel, as some results may be 18+")
+    return message.channel.send(embed)
+  }
 };
 module.exports.help = {
   name: "urban"
