095170d7df110e35ff293abace72102b22461980
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-07 19:49:19 +0100

    Bugfixes (#3; hopefully last one)

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const config = require("../config.json");
+const Discord = require("discord.js");
 
 module.exports = async (client) => {
   console.log(`[INFO] ${client.user.username} has succesfully started!`);
